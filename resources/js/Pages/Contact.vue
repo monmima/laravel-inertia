461a34f8e28f812fbfa154e4111a147942124e0b
monmima <monmima@protonmail.com>
2021-08-18 16:12:19 -0400

    added links to file

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -5,6 +5,8 @@
         <ul>
             <li><inertia-link href="/">Home page</inertia-link></li>
             <li><inertia-link href="/contact">Contact page</inertia-link></li>
+            <li><Link href="/">Home page</Link></li>
+            <li><Link href="/contact">Contact page</Link></li>
         </ul>
     </div>
 </template>
